7781f3a8e995478345c44aabcd6e61154148e938
rsivaraman18 <rsivaraman18@gmail.com>
2024-11-18 19:50:13 +0530

    First Commit

---
+++ learn1.py
@@ -0,0 +1,15 @@
+## 1 - Array Importing 
+import array
+arr = array.array('i',[10,20,30])
+print('Array 1:',arr)
+
+## 2 - Array with Alias name
+import array as ar
+arr2 = ar.array('i',[10,20,30])
+print('Array 2:',arr2)
+
+## 3 - Array all functions
+from array import *
+arr3 = array('i',[10,20,30])
+print('Array 3:',arr3)
+
