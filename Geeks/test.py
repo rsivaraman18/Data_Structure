ae82d4189d7b5a7e27ee9281eb73ecdeac3c507e
rsivaraman18 <rsivaraman18@gmail.com>
2024-11-19 16:36:54 +0530

    Created for Geeks/test.py

--- Geeks/test.py
+++ Geeks/test.py
@@ -1,3 +1,35 @@
 """
+Given an array arr of integers, find all the elements that occur more than once in the array. Return the result in ascending order. If no element repeats, return an empty array.
+pgm1:
+Input: arr[] = [2, 3, 1, 2, 3]
+Output: [2, 3] 
+Explanation: 2 and 3 occur more than once in the given array.
 
+pgm2:
+Input: arr[] = [0, 3, 1, 2] 
+Output: [] 
+Explanation: There is no repeating element in the array, so the output is empty.
+
+Input: arr[] = [2]
+Output: [] 
+Explanation: There is single element in the array. Therefore output is empty.
 """
+
+def findDuplicates(arr):
+    duplicates = []
+    for element in arr:
+        if arr.count(element)>1 and element not in duplicates:
+            duplicates.append(element)
+    return duplicates
+
+            
+        
+
+    
+
+
+print(findDuplicates([2, 3, 1, 2, 3]) ) 
+print(findDuplicates([0, 3, 1, 2] ) ) 
+print(findDuplicates([2]) ) 
+
+3 1 0 4 2 3 1
