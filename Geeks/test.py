b06b2d4e21868921cc8cb46d0965e541cced5c4c
rsivaraman18 <rsivaraman18@gmail.com>
2024-11-19 17:37:06 +0530

    Created for Geeks/test.py

--- Geeks/test.py
+++ Geeks/test.py
@@ -1,18 +1,29 @@
 """
-Given an array arr[] containing only 0s, 1s, and 2s. Sort the array in ascending order.
-pgm1:
-Input: arr[] = [0, 1, 2, 0, 1, 2]
-Output: [0, 0, 1, 1, 2, 2]
+Rearrang Numbers
+pgm1: [9, 4, -2, -1, 5, 0, -5, -3, 2]
+O/P:  [9, -2, 4, -1, 5, -5, 0, -3, 2]
 
-Input: arr[] = [0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1]
-Output: [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2]
+pgm2: [-5, -2, 5, 2, 4, 7, 1, 8, 0, -8]
+O/P:  [5, -5, 2, -2, 4, -8, 7, 1, 8, 0]
+
+pgm3: [9, 5, -2, -1, 5, 0, -5, -3, 2]
+O/P:  [9, -2, 5, -1, 5, -5, 0, -3, 2]
 """
 
-def sort012(arr):
-    result = sorted(arr)
-    return result
 
 
-print(sort012([0, 1, 2, 0, 1, 2]))
 
-print(sort012([0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1]))
+def rearrange(arr):
+    neg_array = []
+    pos_array = []
+
+    for element in arr:
+        if element >=0:
+            pos_array.append(element)
+        else:
+            neg_array.append(element)
+
+
+print(rearrange([9, 4, -2, -1, 5, 0, -5, -3, 2]))
+# print(rearrange([-5, -2, 5, 2, 4, 7, 1, 8, 0, -8]))
+# print(rearrange( [9, 5, -2, -1, 5, 0, -5, -3, 2]))
