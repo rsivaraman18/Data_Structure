9a777ed6ea691b20bc1d06c8c938bff2df1e55e7
rsivaraman18 <rsivaraman18@gmail.com>
2024-11-19 17:17:02 +0530

    Created for Geeks/test.py

--- Geeks/test.py
+++ Geeks/test.py
@@ -1,54 +1,18 @@
 """
-Given an array arr of integers, find all the elements that occur more than once in the array. Return the result in ascending order. If no element repeats, return an empty array.
+Given an array arr[] containing only 0s, 1s, and 2s. Sort the array in ascending order.
 pgm1:
-Input: arr[] = [2, 3, 1, 2, 3]
-Output: [2, 3] 
-Explanation: 2 and 3 occur more than once in the given array.
+Input: arr[] = [0, 1, 2, 0, 1, 2]
+Output: [0, 0, 1, 1, 2, 2]
 
-pgm2:
-Input: arr[] = [0, 3, 1, 2] 
-Output: [] 
-Explanation: There is no repeating element in the array, so the output is empty.
-
-Input: arr[] = [2]
-Output: [] 
-Explanation: There is single element in the array. Therefore output is empty.
+Input: arr[] = [0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1]
+Output: [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2]
 """
 
-# def findDuplicates(arr):
-#     frequenncy = {}
-#     duplicates = []
-
-#     for element in arr:
-#         frequenncy[element] = frequenncy.get(element,0)+1
-    
-
-#     for key,value in frequenncy.items():
-#         if frequenncy[key] >1:
-#             duplicates.append(key)
-#     return sorted(duplicates)
+def sort012(arr):
+    result = sorted(arr)
+    return result
 
 
-    
-def findDuplicates(arr):
-    from collections  import Counter
-    frequency = Counter(arr)
-    print(frequency)
+print(sort012([0, 1, 2, 0, 1, 2]))
 
-    for key,                     in frequency:
-        print(key)
-        # if frequenncy[key] >1:
-            
-
-
-
-            
-        
-
-    
-
-
-# print(findDuplicates([2, 3, 1, 2, 3]) ) 
-# print(findDuplicates([0, 3, 1, 2] ) ) 
-# print(findDuplicates([2]) ) 
-print(findDuplicates( [3 ,1 ,0, 4, 2, 3, 1] ) )
+print(sort012([0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1]))
