32f5667bf2b94f4d74b340a3ff3fe95a1b3d5b7d
rsivaraman18 <rsivaraman18@gmail.com>
2024-11-19 11:23:54 +0530

    Created for Algorithm/SORT_02_Selection_sort.py

--- Algorithm/SORT_02_Selection_sort.py
+++ Algorithm/SORT_02_Selection_sort.py
@@ -3,14 +3,14 @@ Selection Sort is based on the Minimum value moving to the First Index
 """
 
 numbers = [2,5,1,3,4]
-minindex = 0
 
-for i in range(0,len(numbers)):
-    for j in range(1,len(numbers)):
-        if numbers[minindex] > numbers[j]:
-            minindex = j
-    
-    numbers[j],numbers[minindex] = numbers[minindex],numbers[j]
-    print(numbers)
+
+for position in range(0,len(numbers)):
+    minindex = position
+    for i in range(position,len(numbers)):
+        if numbers[i] < numbers[minindex]:
+            minindex = i
+    numbers[minindex],numbers[position] = numbers[position],numbers[minindex]
+print(numbers)
 
 
