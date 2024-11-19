7e53ff6340728007673577f962f87f173da36747
rsivaraman18 <rsivaraman18@gmail.com>
2024-11-19 20:42:35 +0530

    Created for Geeks/test.py

--- Geeks/test.py
+++ Geeks/test.py
@@ -1,41 +1,9 @@
 """
-Majority Element
-
-prgm1 : 
-Input: arr[] = [3, 1, 3, 3, 2]
-Output: 3
-
-Input: arr[] = [7]
-Output: 7
-
-Input: arr[] = [2, 13]
-Output: -1
+Smallest Positive Integer
 """
 
-def majorityElement(arr):
-    if len(arr) == 1:
-        return arr[0]
-
-
-    d = {}
-    for element in arr:
-        d[element] = d.get(element,0) + 1
-    
-    maxi = 0
-    for key,value in d.items():
-        if d[key] >maxi:
-            maxi = d[key]
-
-    if maxi != 0:
-        print('mm',maxi)
-        return maxi
-    else:
-        return -1 
-    print(d)
+def 
 
 
 
-
-print(majorityElement([3, 1, 3, 3, 2]))
-print(majorityElement( [7]))
-print(majorityElement([2, 13]))
+print()
