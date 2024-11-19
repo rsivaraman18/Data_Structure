8ae270532dd22d9af62c9ae528fed2f8eb79df3a
rsivaraman18 <rsivaraman18@gmail.com>
2024-11-19 21:02:38 +0530

    Created for Geeks/test.py

--- Geeks/test.py
+++ Geeks/test.py
@@ -2,8 +2,21 @@
 Smallest Positive Integer
 """
 
-def 
+def missingNumber(arr):
+    mini = 9999999
+    for element in arr:
+        if (element >=0) :
+            
+            if element < mini:
+                mini = element
+    return mini
+    
+            
+        
 
 
 
-print()
+
+print(missingNumber([2, -3, 4, 1, 1, 7]))
+print(missingNumber([5, 3, 2, 5, 1]))
+print(missingNumber([-8, 0, -1, -4, -3]))
